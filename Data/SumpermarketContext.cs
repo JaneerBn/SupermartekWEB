5891b00919656c19a6d144844008c5242967a2b0
JaneerBn <159595714+JaneerBn@users.noreply.github.com>
2024-11-09 01:17:03 -0500

    Segundo commit

---
+++ Data/SumpermarketContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Data
+{
+    public class SumpermarketContext : DbContext
+    {
+
+        public SumpermarketContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<PayMode> PayModes { get; set; }
+        public DbSet<Provider> Providers { get; set; }
+
+         public DbSet<User> Users { get; set; }
+
+    }
+}
